e1bcea4b159867f1cffbeb03e270ae5e0d700a9c
Moritz Haarmann <m.haarmann@gmail.com>
2009-11-23 15:18:45 +0100

    * bootstrapping task in Rakefile added. * Empty redis won't let anything crash

--- Rakefile
+++ Rakefile
@@ -5,3 +5,13 @@ task :tests do |t|
   require 'main'
   Dir["./tests/*.rb"].each {|x| require x }
 end
+
+desc "bootstraps redis"
+task :bootstrap do |t|
+  require 'redis'
+  r = Redis.new
+  # bootstrapping log container
+  r.push_tail('logs', nil)
+  r.push_tail('blog_index',nil)
+  r.push_tail('tags',nil)
+end
--- tools/wp-import.rb
+++ tools/wp-import.rb
@@ -12,7 +12,6 @@ begin
   @prefix =  ARGV[4]
   res = dbh.query("Select #{@prefix}posts.id as id, #{@prefix}posts.post_title as title, #{@prefix}posts.post_content as content, #{@prefix}posts.post_date as date, #{@prefix}posts.post_type as type from #{@prefix}posts")
   res.each_hash do |post|
-    puts "post."
     p = Post.new
     p.title = post['title']
     p.slug = post['title']
@@ -21,13 +20,13 @@ begin
     @taglist = []
     tags = dbh.query("select #{@prefix}terms.name as tag from #{@prefix}terms,#{@prefix}term_taxonomy,#{@prefix}term_relationships where #{@prefix}term_relationships.object_id = #{post['id']} and #{@prefix}term_taxonomy.term_taxonomy_id = #{@prefix}term_relationships.term_taxonomy_id and #{@prefix}terms.term_id = #{@prefix}term_taxonomy.term_id")
     tags.each_hash do |tag|
-      puts "t."
       @taglist << tag['tag']
     end
     p.tags = @taglist
+    puts post['type']
     if post['type'] == 'post'
       p.save_post
-    else
+    elsif post['type'] == 'page'
       p.save_page
     end
   end
