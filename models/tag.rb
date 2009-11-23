7e4d59e5110ec9ec1dc57fbc4e983b1ddea77475
Moritz Haarmann <m.haarmann@gmail.com>
2009-11-23 20:08:34 +0100

    * added tag caching

--- models/tag.rb
+++ models/tag.rb
@@ -1,5 +1,13 @@
 class Tag
   def self.get_top_tags
-    Post.srv.keys("tag_*").sort_by{|tagname| Post.srv.list_length(tagname)}.reverse[0,10]
+      rebuild_top_tags if !Post.srv.key?("top_tags") 
+    YAML.load(Post.srv["top_tags"])
+  end
+  
+  def self.rebuild_top_tags
+    puts "building tag."
+    Post.srv.delete("top_tags")
+    data = Post.srv.keys("tag_*").sort_by{|tagname| Post.srv.list_length(tagname)}.reverse[0,10]
+    Post.srv['top_tags'] = data.to_yaml
   end
 end
